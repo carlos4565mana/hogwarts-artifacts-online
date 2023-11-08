e765527a8981129800d7083441863c00c45f7fea
carlos4565mana <carloscal61@gmail.com>
2023-11-07 22:07:52 -0200

    Created Project

---
+++ src/main/java/com/carlos/springbootcurse/SpringBootCurseApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.springbootcurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurseApplication.class, args);
+	}
+
+}
